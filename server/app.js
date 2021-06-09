dc5ba37eecc39d19d1daafda18d3ecdba1c519b9
jishu-yadav <jishuyadavcute88@gmail.com>
2021-06-09 17:28:00 +0530

    Alumni Login

--- server/app.js
+++ server/app.js
@@ -6,32 +6,36 @@ const mongoose = require('mongoose')
 const app = express()
 const port = 3000
 
-const Student = require('./models/studentModel')
 const Alumni = require('./models/alumniModel')
+
 
 dotenv.config({path:'./config.env'})
 
 require('./db/conn') 
 
 //Middleware
+app.use(express.json())
+app.use(require('./router/auth'));
+
+
 const middleware=(req,res,next)=>{
     console.log(`Hello my middleware`);
     next();
 }
 
 
-const urlencodedParser = bodyParser.urlencoded({ extended: false })
+//const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Navigation
 app.get('/', (req, res)=> {
     res.send(`home`);
 })
 
-app.get('/register',middleware, (req, res)=> {
+app.get('/register', (req, res)=> {
     res.send(`register`);
 })
 
-app.post('/register', urlencodedParser, [
+/*app.post('/register', urlencodedParser, [
     check('firstname', 'The firstname must be 3+ characters long')
         .exists()
         .isLength({ min: 3 }),
@@ -50,7 +54,7 @@ app.post('/register', urlencodedParser, [
         //res.render('register', {alert})
     }
 
-})
+})*/
 app.get('/login',(req,res)=>{
     res.send(`Login`)
 })
---
+++ server/router/auth.js
@@ -0,0 +1,86 @@
+const express = require('express');
+const router= express.Router();
+require ('../db/conn');
+const Alumni = require('../models/alumniModel')
+router.get('/',(req,res)=>{
+    //res.send(`Hello world from the server router js`);
+    Alumni.find({ })
+      .then((data)=>{
+          console.log(data);
+          res.json(data);
+      })
+      .catch((err)=>{
+          console.log(err);
+      })
+
+})
+router.post('/register',(req,res)=>{
+  //  console.log(req.body);
+    //console.log(req.body.firstName);
+    const{firstName, lastName, residence, password ,phone, email, 
+        linkedin, github,year, roll,dept,
+         branch,occupation, workingAt, position,
+          achievements}=req.body
+
+          console.log(req.body.firstName);
+          console.log(req.body.lastName);
+          console.log(req.body.residence);
+          console.log(req.body.password);
+          console.log(req.body.phone);
+          console.log(req.body.email);
+          console.log(req.body.linkedin);
+          console.log(req.body.github);
+          console.log(req.body.year);
+          console.log(req.body.roll);
+          console.log(req.body.dept);
+          console.log(req.body.branch);
+          console.log(req.body.workingAt);
+          console.log(req.body.position);
+          console.log(req.body.achievements);
+
+
+  if(!firstName || !lastName || !residence || !password || !phone || !email || !linkedin || !github || !year || !roll || !dept || !branch || !occupation || !workingAt || !position || !achievements){
+        return res.status(422).json({error:"No field can be empty"});
+    }
+    Alumni.findOne({email:email})
+    .then((alumniExist)=>{
+        if(alumniExist){
+            return res.status(422).json({error:"Alumni already registered"})
+        }
+        const alumni = new Alumni({firstName, lastName, residence, password ,phone, email, linkedin, github,year, roll,dept, branch,occupation, workingAt, position, achievements})
+    alumni.markModified();
+    alumni.save().then(()=>{
+        res.status(201).json({message:'Alumni registered'})
+    }).catch((err)=>res.status(500).json({error:"Failed to register"}));
+    }).catch((err)=>{console.log(err)})
+
+
+})
+
+//login route
+router.post('/login',async (req,res)=>{
+    //console.log(req.body);
+    //res.json({message:"welcm back"});
+    try{
+        const {email,password}=req.body;
+
+        if(!email || !password){ 
+            return res.status(400).json({error:"Plz fill the credentials properly"})
+        }
+        const alumniLogin = await Alumni.findOne({email:email});
+        console.log(alumniLogin);
+        if(!alumniLogin){
+            res.status(400).json({error:"Alumni Login error"});
+        }else{
+            
+        res.json({message:"Alumni Logged in Successfully"});
+ 
+        }
+   }catch(err){
+        console.log(err);
+    }
+
+})
+
+
+module.exports=router; 
