6c0270da4760bf20d4dc23891cc757aa813997e0
Priyanka7011 <pk3201134@gmail.com>
2021-06-10 22:44:27 +0530

    filter

--- client/src/homepage/home.js
+++ client/src/homepage/home.js
@@ -11,10 +11,10 @@ import {faFacebook,faLinkedin,faTwitter,faInstagram, faMailchimp, faGithub} from
 const Home=()=>{
  
     const [alumnis,setAlumnis]=useState([])
- 
+    const [searchTerm,setSearchTerm]=useState("")
   
-    
-    const getAlumnis=async()=>{
+   
+    const getAlumnis=async(filterBranch,filterDept)=>{
        /*axios.get('/api')
        .then((response)=>{
            console.log("DATA")
@@ -22,6 +22,7 @@ const Home=()=>{
            setAlumnis(prevAlumni=>([...prevAlumni,...response.data]))
            console.log("alumni data")
            console.log(alumnis)
+          
            
        })
        .catch((err)=>{
@@ -37,13 +38,44 @@ const Home=()=>{
       
        console.log(alumnis)
        
+       
+      if(filterDept || filterBranch)
+      {
+        if(filterBranch!==' ' && filterDept!==' '){
+            console.log(filterBranch, filterDept)
+            const updatedItems=data.filter((curItem)=>{
+                return curItem.branch === filterBranch && curItem.dept === filterDept
+            });
+            setAlumnis(updatedItems)
+          }
+           if(filterDept === ' '){
+            console.log(filterBranch, filterDept)
+            const updatedItems=data.filter((curItem)=>{
+                return curItem.branch === filterBranch 
+            });
+            setAlumnis(updatedItems)
+    
+          }else if(filterBranch === ' '){
+              console.log('dept')
+            console.log(filterBranch, filterDept)
+            const updatedItems=data.filter((curItem)=>{
+                return curItem.dept === filterDept
+            });
+            setAlumnis(updatedItems)
+    
+          }
+    
+      }
+
+
+       
     }
     useEffect(()=>{
         getAlumnis();
     },[]);
     
    
-  
+    
     
     return(
             <div>
@@ -53,7 +85,13 @@ const Home=()=>{
                 <div class='container py-5 my-5'>
                 <div class='flex-row d-flex justify-content-center align-items-center'>
                     <div class='searchItem d-flex flex-row align-items-center'>
-                        <input class='form-control' placeholder='search..' type='text'></input>
+                        <input class='form-control' 
+                        placeholder='search..' 
+                        type='text'
+                        onChange={event=>{
+                            setSearchTerm(event.target.value)
+                        }}
+                        ></input>
                         <div class='searchIcon'>
                         <FontAwesomeIcon icon={faSearch}/>
                         </div>
@@ -64,28 +102,36 @@ const Home=()=>{
                             Filter By
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Department
-                            <input type="radio" id="btech" name="gender" value="btech"/>
+                            <a class="dropdown-item d-flex flex-row justify-content-between" href="#">Department
+                            <input type="radio" id="btech" name="gender" value="btech"  onClick={()=>getAlumnis(' ','B.Tech')}/>
                             <label for="btech">B.Tech</label>
-                            <input type="radio" id="mtech" name="gender" value="mtech"/>
+                            <input type="radio" id="mtech" name="gender" value="mtech"  onClick={()=>getAlumnis(' ','M.Tech')}/>
                             <label for="mtech">M.Tech</label>
-                            <input type="radio" id="phd" name="gender" value="phd"/>
+                            <input type="radio" id="phd" name="gender" value="phd"  onClick={()=>getAlumnis(' ','phD')}/>
                             <label for="phd">phD</label>
                             </a>
                             <a class="dropdown-item" href="#">Passout Year</a>
                             <a class="dropdown-item" href="#">Branch
-                            <input type="radio" id="cse" name="gender" value="cse"/>
+                            <input type="radio" id="cse" name="branch" value="cse" onClick={()=>getAlumnis('CSE',' ')}/>
                             <label for="cse">CSE</label>
-                            <input type="radio" id="ece" name="gender" value="ece"/>
+                            <input type="radio" id="ece" name="branch" value="ece"  onClick={()=>getAlumnis('ECE',' ')}/>
                             <label for="ece">ECE</label>
-                            </a>
+                             </a>
+                             
                         </div>
                     </div>
                  </div>
                    
                     <div class='d-flex flex-wrap my-5 justify-content-between'>
                      {
-                         alumnis.map((curitem)=>{
+                         alumnis.filter((val)=>{
+                             if(searchTerm===""){
+                                 return val;
+                             }
+                             else if(val.firstName.toLowerCase().includes(searchTerm.toLowerCase())){
+                                  return val;
+                             }
+                         }).map((curitem)=>{
                              return(
                                 <div class="card" >
                                 <img class="card-img img-fluid rounded" src={girlImage} alt="Card image cap"></img>
