2cad3d2a0f56063341cac7dbb2bf3766ea9db24f
jishu-yadav <jishuyadavcute88@gmail.com>
2021-07-12 22:51:33 +0530

    desktop2_done

--- client/src/homepage/homepage.js
+++ client/src/homepage/homepage.js
@@ -8,10 +8,22 @@ const Home=()=>{
         
         <div class='ContainerHolder' >
             <Navbar/>
-            <div >
-            <h1>Home</h1>
+            
+            <div class="alumni">
+                <img   height={530} width={1536} />
+
+                <div className='new-line'  width={300}>
+                     <h1 class="Alumni_heading1"> Alumni @ IIITG</h1>
+                     <h2  class="Alumni_heading2">{"\n"}Welcome! Explore the many{"\n"}
+                     ways to connect with the college{"\n"}
+                     and our 23,000 alumni.{"\n"}
+                     <canvas id="my-canvas" width="400" height="2" ></canvas>
+                     </h2>
+                     
+                     
+                </div> 
             </div>
-          
+
            <Footer/>
         </div>
     )
